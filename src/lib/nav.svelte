a52e28ab24d80ccb00ef5aa67c580532c64b8053
Erwin de Vries <esdevries313@gmail.com>
2024-06-30 20:11:27 +0200

    add font

---
+++ src/lib/nav.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">Erwin de Vries</a>
+
+    <div class="links">
+        <a href="/portfolio">portfolio</a>
+    </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
+    import Nav from '$lib/nav.svelte'
     import '../styles/global.css'
 </script>
+
+<header>
+    <Nav />
+</header>
 
 <main>
     <slot></slot>
